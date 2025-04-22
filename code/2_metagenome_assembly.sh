2468de973bcbef09c32e917ebbc83c3ba9c6be71
Agnes Niemi <niemi@rackham1.uppmax.uu.se>
2025-04-22 09:01:23 +0200

    completed the metagenome assembly code

--- code/2_metagenome_assembly.sh
+++ code/2_metagenome_assembly.sh
@@ -2,8 +2,8 @@
 #SBATCH -A uppmax2025-3-3
 #SBATCH -M snowy
 #SBATCH -p core
-#SBATCH -n 2
-#SBATCH -t 08:00:00
+#SBATCH -n 8
+#SBATCH -t 10:00:00
 #SBATCH -J metagenome_assembly
 
 #SBATCH --mail-type=ALL
@@ -13,10 +13,17 @@
 
 # Load modules
 module load bioinfo-tools
-module load ....
+module load megahit
+
 # Your commands
-<Command
-1...>
-_
-<Command
-2...>
+
+dna_SRR4342129_forward=/home/niemi/Genome-Analysis/data/dna/raw_data/SRR4342129_1.paired.trimmed.fastq.gz
+dna_SRR4342129_reverse=/home/niemi/Genome-Analysis/data/dna/raw_data/SRR4342129_2.paired.trimmed.fastq.gz
+dna_SRR4342133_forward=/home/niemi/Genome-Analysis/data/dna/raw_data/SRR4342133_1.paired.trimmed.fastq.gz
+dna_SRR4342133_reverse=/home/niemi/Genome-Analysis/data/dna/raw_data/SRR4342133_2.paired.trimmed.fastq.gz
+
+
+megahit -1 $dna_SRR4342129_forward -2 $dna_SRR4342129_reverse -t 4 -o /home/niemi/Genome-Analysis/analyses/dna/02_metagenome_assembly/megahit_results/res_SRR4342129 --k-min 21 --k-max 81 --k-step 20
+megahit	-1 $dna_SRR4342133_forward -2 $dna_SRR4342133_reverse -t 4 -o /home/niemi/Genome-Analysis/analyses/dna/02_metagenome_assembly/megahit_results/res_SRR4342133 --k-min 21 --k-max 81 --k-step 20
+
+
