13f1f4964a574e20f8c31e4603f8014fda544b96
Agnes Niemi <niemi@rackham1.uppmax.uu.se>
2025-05-02 13:41:53 +0200

    corrected paths to binning output

--- code/5_binning.sh
+++ code/5_binning.sh
@@ -20,8 +20,8 @@ final_contigs_SRR4342129=/home/niemi/Genome-Analysis/analyses/dna/02_metagenome_
 final_contigs_SRR4342133=/home/niemi/Genome-Analysis/analyses/dna/02_metagenome_assembly/megahit_results/res_SRR4342133/final.contigs.fa
 
 # Where the output bins should be located
-output_SRR4342129=/home/niemi/Genome-Analysis/analyses/dna/04_binning/bins_SRR4342129/bin
-output_SRR4342133=/home/niemi/Genome-Analysis/analyses/dna/04_binning/bins_SRR4342133/bin
+output_SRR4342129=/home/niemi/Genome-Analysis/analyses/dna/05_binning/bins_SRR4342129/bin
+output_SRR4342133=/home/niemi/Genome-Analysis/analyses/dna/05_binning/bins_SRR4342133/bin
 
 # Creating depth files 
 jgi_summarize_bam_contig_depths --outputDepth depth_SRR4342129.txt /home/niemi/Genome-Analysis/analyses/dna/04_bwa/aln_sorted_SRR4342129.bam
